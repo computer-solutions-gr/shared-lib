a6c26fc8257060c8fac7988a6fa29598daea6f20
Stavros Pitoglou <s.pitoglou@csl.gr>
2023-12-24 12:55:12 +0200

    First Test

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='cssa_shared-lib',
+      version='0.1',
+      description='CSSA common libraries',
+      url='https://github.com/computer-solutions-gr/shared-lib',
+      author='Stavros Pitoglou',
+      author_email='s.pitoglou@csl.gr',
+      license='GNU',
+      packages=['cssalib'],
+    #   test_suite='nose.collector',
+    #   tests_require=['nose'],
+      install_requires=[
+          'requests',
+          'loguru',
+          'scikit-learn',
+          'numpy',
+          'matplotlib'
+      ],
+      zip_safe=False)
